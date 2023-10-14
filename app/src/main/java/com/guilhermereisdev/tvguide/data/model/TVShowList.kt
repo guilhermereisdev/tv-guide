ec2b52ea43e15fa6a18fecdcab83ddc1aece68fe
Guilherme Reis <guilhermereis2009@hotmail.com>
2023-10-14 19:52:16 -0300

    Cria os models

---
+++ app/src/main/java/com/guilhermereisdev/tvguide/data/model/TVShowList.kt
@@ -0,0 +1,15 @@
+package com.guilhermereisdev.tvguide.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class TVShowList(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val TVShows: List<TVShow>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
